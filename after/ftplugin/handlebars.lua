203d03437d34d62f9765133f19c236e72b556c1f
Kati Fredman <kati@katvef.fi>
2026-08-02 21:20:33 +0300

    Improved handlebars support

--- after/ftplugin/handlebars.lua
+++ after/ftplugin/handlebars.lua
@@ -1,4 +1,4 @@
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.treesitter.language.register("handlebars", { "handlebars" })
-vim.schedule(function() vim.bo.syntax = "handlebars" end)
+vim.schedule(function() vim.bo.syntax = "html" end)
