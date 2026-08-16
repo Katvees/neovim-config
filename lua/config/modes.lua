f5092292e70dae83315e70462f295a66e1e8d48a
Kati Fredman <kati@katvef.fi>
2026-08-16 15:51:42 +0300

    Changed modes colors

--- lua/config/modes.lua
+++ lua/config/modes.lua
@@ -1,15 +1,24 @@
-local palette = require("nightfox.palette.nightfox").palette
+local pal = require("nightfox.palette.nightfox").palette
 require('modes').setup({
 	colors = {
 		bg = "",
-		copy = palette.magenta.base,
-		delete = palette.red.bright,
-		format = palette.orange.base,
-		insert = palette.cyan.bright,
-		replace = palette.orange.base,
-		visual = palette.blue.base,
+		copy = pal.cyan.bright,
+		delete = pal.red.bright,
+		change = pal.orange.bright,
+		format = pal.orange.base,
+		insert = pal.bg3,
+		replace = pal.orange.base,
+		visual = pal.bg3,
 	},
-	line_opacity = 0.3,
+	line_opacity = {
+		copy = 0.4,
+		delete = 0.3,
+		change = 0.3,
+		format = 0.3,
+		insert = 1,
+		replace = 0.2,
+		visual = 1,
+	},
 	set_cursor = false,
 	set_cursorline = true,
 	set_number = false,
